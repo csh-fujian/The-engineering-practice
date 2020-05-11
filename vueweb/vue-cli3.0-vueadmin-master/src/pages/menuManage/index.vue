04a20f4675fb695ae4561d1d8a33d7298c1116a8
Holdthedoor1 <51039306+HuangdaLiu@users.noreply.github.com>
2020-05-11 22:25:02 +0800

    接口文档

--- vueweb/vue-cli3.0-vueadmin-master/src/pages/menuManage/index.vue
+++ vueweb/vue-cli3.0-vueadmin-master/src/pages/menuManage/index.vue
@@ -1,56 +1,55 @@
 <template>
-    <div>
-        <ul>
-            <el-checkbox-group v-model="checkdfirstLevelPage" >
-                <ul>
-                    <li  v-for="item1 in firstLevelPage">
-                        <el-checkbox :label="item1" :key="item1"><strong >{{item1}}</strong></el-checkbox>
-                            <ul>
-                                <el-checkbox-group v-model="checkedCourseDetails" >
-                                    <ul>
-                                        <li  v-for="item2 in courseDetails">
-                                            <el-checkbox   :label="item2" :key="item2">{{item2}}</el-checkbox>
-                                                <el-checkbox-group v-model="checkedCourseManage" >
-                                                    <el-checkbox v-for="item3 in courseManagers" :label="item3" :key="item3">{{item3}}</el-checkbox>
-                                                </el-checkbox-group>
-                                        </li>
-                                    </ul>
-                                </el-checkbox-group>
-                            </ul>
-                        <el-divider></el-divider>
-                    </li>
-                </ul>
-            </el-checkbox-group>
-        </ul>
-        <el-button type="primary" @click="">立即创建</el-button>
-    </div>
+<div class="block">
+    <span class="demonstration">选择院校</span>
+    <el-cascader
+        v-model="value"
+        :options="options"
+        @change="handleChange"></el-cascader>
+</div>
 </template>
 <script>
     export default {
         data() {
             return {
-                checkedCourseManage: ['教师创建课程', '申请加入'],
-                checkedCourseDetails: ['课程管理1', '课程详情'],
-                courseManagers: ['课程管理','教师创建课程', '申请加入', '批准加入', '删除课程'],
-                courseDetails: ['课程管理1','课程详情', '资源', '活动'],
-                firstLevelPage:['内容发现','我的课程'],
-                checkdfirstLevelPage:['内容发现']
+                value: [],
+                options: [{
+                    value: '福州大学',
+                    label: '福州大学',
+                    children: [{
+                        value: '数学与计算机科学学院',
+                        label: '数学与计算机科学学院'
+                    }, {
+                        value: '经济与管理学院',
+                        label: '经济与管理学院',
+                    }]
+                }, {
+                    value: '福建师范大学',
+                    label: '福建师范大学',
+                    children: [{
+                        value: '软件学院',
+                        label: '软件学院',
+                    }, {
+                        value: '音乐学院',
+                        label: '音乐学院',
+                    }]
+                }, {
+                    value: '厦门大学',
+                    label: '厦门大学',
+                    children: [{
+                        value: '国际关系学院',
+                        label: '国际关系学院',
+                    }, {
+                        value: '管理学院',
+                        label: '管理学院',
+                    }]
+                }]
             };
         },
         methods: {
-            courseManage(value) {
-            },
-            courseDetail(value){
+            handleChange(value) {
+                console.log(value);
+                alert(value[1])
             }
-        },
-        created() {
-            this.$http.get('http://localhost:3003/api')
-                .then((data) => {
-                    alert(123)
-                    alert(data.body)
-                }, () => {
-                    console.log('这里是用了vue-source,后端没有接口')
-                })
         }
     };
 </script>
--- vueweb/vue-cli3.0-vueadmin-master/src/pages/user-manage/user-manage-test/index.vue
+++ vueweb/vue-cli3.0-vueadmin-master/src/pages/user-manage/user-manage-test/index.vue
@@ -13,7 +13,7 @@
             style="width: 100%">
       <el-table-column
               label="学号"
-              prop="StudentId">
+              prop="id">
       </el-table-column>
       <el-table-column
               label="姓名"
@@ -25,7 +25,7 @@
       </el-table-column>
       <el-table-column
               label="身份"
-              prop="identity">
+              prop="role">
       </el-table-column>
       <el-table-column
               label="电话"
@@ -65,33 +65,69 @@
           :visible.sync="dialogVisible"
           width="60%"
           :before-close="handleClose">
-          <el-form ref="ro" :model="addData" label-width="80px">
+          <el-form ref="ro" :model="user" label-width="80px">
               <el-form-item label="学号">
-                  <el-input v-model="addData.StudentId"></el-input>
+                  <el-input v-model="user.id"></el-input>
               </el-form-item>
               <el-form-item label="姓名">
-                  <el-input v-model="addData.name"></el-input>
+                  <el-input v-model="user.name"></el-input>
               </el-form-item>
               <el-form-item label="性别">
-                  <el-input v-model="addData.sex"></el-input>
+                  <el-input v-model="user.sex"></el-input>
               </el-form-item>
               <el-form-item label="身份">
-                  <el-input v-model="addData.identity"></el-input>
+                  <el-input v-model="user.role"></el-input>
               </el-form-item>
               <el-form-item label="电话号码">
-                  <el-input v-model="addData.phoneNumber"></el-input>
+                  <el-input v-model="user.phoneNumber"></el-input>
               </el-form-item>
               <el-form-item label="学校">
-                  <el-input v-model="addData.School"></el-input>
+                  <el-input v-model="user.School"></el-input>
               </el-form-item>
               <el-form-item label="学院">
-                  <el-input v-model="addData.College"></el-input>
+                  <el-input v-model="user.College"></el-input>
               </el-form-item>
               <el-form-item label="创建时间">
-                  <el-input v-model="addData.creatTime"></el-input>
+                  <el-input v-model="user.creatTime"></el-input>
               </el-form-item>
               <el-form-item>
                   <el-button type="primary" @click="onSubmit()">确定</el-button>
+                  <el-button>取消</el-button>
+              </el-form-item>
+          </el-form>
+      </el-dialog>
+       <el-dialog
+          title="编辑用户"
+          :visible.sync="dialogVisible2"
+          width="60%"
+          :before-close="handleClose">
+          <el-form ref="ro" :model="user" label-width="80px">
+              <el-form-item label="学号">
+                  <el-input v-model="user.id"></el-input>
+              </el-form-item>
+              <el-form-item label="姓名">
+                  <el-input v-model="user.name"></el-input>
+              </el-form-item>
+              <el-form-item label="性别">
+                  <el-input v-model="user.sex"></el-input>
+              </el-form-item>
+              <el-form-item label="身份">
+                  <el-input v-model="user.role"></el-input>
+              </el-form-item>
+              <el-form-item label="电话号码">
+                  <el-input v-model="user.phoneNumber"></el-input>
+              </el-form-item>
+              <el-form-item label="学校">
+                  <el-input v-model="user.School"></el-input>
+              </el-form-item>
+              <el-form-item label="学院">
+                  <el-input v-model="user.College"></el-input>
+              </el-form-item>
+              <el-form-item label="创建时间">
+                  <el-input v-model="user.creatTime"></el-input>
+              </el-form-item>
+              <el-form-item>
+                  <el-button type="primary" @click="edit()">确定</el-button>
                   <el-button>取消</el-button>
               </el-form-item>
           </el-form>
@@ -107,48 +143,21 @@ export default {
         return {
             dialogVisible: false,
             dialogVisible2: false,
-            addData: {
-                StudentId: '190327064',
+            user: {
+                id: 190327064,
                 name: '王小虎',
                 sex: '男',
                 identity: '教师',
-                phoneNumber: '1335855555',
-                School: '福州大学',
+                role: '1335855555',
+                school: '福州大学',
                 College: '数学与计算机科学',
                 creatTime: '2020-04-20'
             },
             tableData: [{
-                StudentId: '190327064',
+                id: 190327064111,
                 name: '王小虎',
                 sex: '男',
-                identity: '教师',
-                phoneNumber: '1335855555',
-                School: '福州大学',
-                College: '数学与计算机科学',
-                creatTime: '2020-04-20'
-            }, {
-                StudentId: '190327064',
-                name: '王小虎',
-                sex: '男',
-                identity: '教师',
-                phoneNumber: '1335855555',
-                School: '福州大学',
-                College: '数学与计算机科学',
-                creatTime: '2020-04-20'
-            }, {
-                StudentId: '190327064',
-                name: '王小虎',
-                sex: '男',
-                identity: '教师',
-                phoneNumber: '1335855555',
-                School: '福州大学',
-                College: '数学与计算机科学',
-                creatTime: '2020-04-20'
-            }, {
-                StudentId: '190327064',
-                name: '王小虎',
-                sex: '男',
-                identity: '教师',
+                role: '教师',
                 phoneNumber: '1335855555',
                 School: '福州大学',
                 College: '数学与计算机科学',
@@ -159,14 +168,20 @@ export default {
     },
     methods: {
         handleEdit(index, row) {
-            alert(this.tableData.StudentId)
+            alert(row.id)
+            this.user = row
             this.dialogVisible2 = true
             console.log(index, row)
         },
+        edit(){
+            console.log(this.user)
+            this.dialogVisible2 = false
+ 
+        },
         handleDelete(index, row) {
-            this.addData = row
-            console.log(this.addData)
-            this.$axios.post('http://localhost:8080/webrole/deleteuser',this.addData).then(function(resp) {
+            this.user = row
+            console.log(this.user)
+            this.$axios.post('http://localhost:8080/webuser/deleteuser',this.user).then(function(resp) {
                 console.log(resp)
             })
         },
@@ -177,10 +192,10 @@ export default {
             this.dialogVisible = false
         },
         onSubmit(){
-            console.log(this.addData)
+            console.log(this.user)
             this.dialogVisible = false
              const _this = this
-             this.$axios.post('http://localhost:8080/webrole/addrole',this.addData).then(function(resp) {
+             this.$axios.post('http://localhost:8080/webuser/adduser',this.user).then(function(resp) {
                  console.log(resp)
              })
         },
@@ -190,7 +205,7 @@ export default {
     },
     created() {
         const _this = this
-        this.$axios.get('http://localhost:8080/webrole/findrole').then(function(resp) {
+        this.$axios.get('http://localhost:8080/webuser/finduser').then(function(resp) {
             _this.tableData = resp.data
             // alert(321)
             //这个url要改下
